27c480999fea578067ab1ade6ccdf5c365b2d23c
VicenteLucero <vjlucero@miuandes.cl>
2019-05-16 12:09:42 -0400

    give up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+				 :omniauthable, :omniauth_providers => [:facebook]
   	has_many :user_tickets	
 	has_many :tickets, :through => :user_tickets
 
@@ -7,6 +12,12 @@ class User < ApplicationRecord
 	validates :phone, length: {minimum: 9, maximum: 12}, allow_blank: true
   	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "assword must be between 8 to 12 alphanumeric characters"}
 
-
-
+	def self.from_omniauth(auth)
+		where(provider:auth.provider, uid:auth.uid).first_or_create do |user|
+			user.provider=auth.provider
+			user.uid = auth.uid
+			user.email = auth.info.name
+			user.password = Devise.friendly_token[0,20]
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise :omniauth_callbacks => "callbacks"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+
 
   root :to => 'sessions#new'
 
@@ -9,5 +12,7 @@ Rails.application.routes.draw do
 
   get '/sign_in' => 'registrations#new', as: :registrations
   post '/sign_in' => 'registrations#create', as: :sign_in
+
+
 
 end
